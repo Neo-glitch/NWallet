347df2900f175a741a35a4808a72a02dda195709
Nweke Onyekachukwu <55864433+Neo-glitch@users.noreply.github.com>
2022-07-05 13:27:36 +0100

    set up for hom screen started

--- app/src/main/java/com/neo/nwallet/screens/main/home/HomeViewModel.kt
+++ app/src/main/java/com/neo/nwallet/screens/main/home/HomeViewModel.kt
@@ -2,7 +2,8 @@ package com.neo.nwallet.screens.main.home
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel: ViewModel() {
+class HomeViewModel @Inject constructor(): ViewModel() {
 }
